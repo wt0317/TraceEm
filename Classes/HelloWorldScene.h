83117e5f384cfe979877c43666156121fc26f0b3
Wilson Tran <wt.0317@gmail.com>
2014-11-28 23:36:32 -0500

    enable logging, track touches

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -3,6 +3,9 @@
 
 #include "cocos2d.h"
 
+using namespace std;
+using namespace cocos2d;
+
 class HelloWorld : public cocos2d::Layer
 {
 public:
@@ -12,8 +15,11 @@ public:
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();  
     
-    // a selector callback
-    void menuCloseCallback(cocos2d::Ref* pSender);
+    virtual void onTouchesBegan(const vector<Touch*>& touches, Event* event);
+	virtual void onTouchesMoved(const vector<Touch*>& touches, Event* event);
+	virtual void onTouchesEnded(const vector<Touch*>& touches, Event* event);
+
+	void drawCircle(const Point& location);
     
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
--- proj.android/jni/Application.mk
+++ proj.android/jni/Application.mk
@@ -1,7 +1,7 @@
 APP_STL := c++_static
 NDK_TOOLCHAIN_VERSION=clang
 
-APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char
+APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char -DCOCOS2D_DEBUG=1
 APP_LDFLAGS := -latomic
 
 
